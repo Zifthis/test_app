a86f7124feaab70f32f4ec73250375de6a371af3
matija <matija.kovacevic@q.agency>
2022-11-02 12:18:26 +0100

    removing duplicate code

--- lib/common/network/api_client/api_client.dart
+++ lib/common/network/api_client/api_client.dart
@@ -1,11 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
-import 'package:test_app/common/network/api_const.dart';
 import 'package:test_app/feature/popular_movies/data/models/get_popular_response.dart';
 
 part 'api_client.g.dart';
 
-@RestApi(baseUrl: ApiConst.baseUrl)
+@RestApi()
 abstract class ApiClient {
   factory ApiClient(Dio dio, {String baseUrl}) = _ApiClient;
 
--- lib/common/network/api_client/api_client.g.dart
+++ lib/common/network/api_client/api_client.g.dart
@@ -12,9 +12,7 @@ class _ApiClient implements ApiClient {
   _ApiClient(
     this._dio, {
     this.baseUrl,
-  }) {
-    baseUrl ??= 'https://api.themoviedb.org/3';
-  }
+  });
 
   final Dio _dio;
 
