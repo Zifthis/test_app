c4ad72519f78f094c7c4f65742b1aafb144d9aef
matija <matija.kovacevic@q.agency>
2022-11-04 15:32:06 +0100

    bottom sheet added

--- lib/common/presentation/home.dart
+++ lib/common/presentation/home.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:test_app/feature/movie_details/domain/notifier/bottom_sheet_provider.dart';
 import 'package:test_app/feature/popular_movies/domain/notifier/movie_provider.dart';
 import 'package:test_app/feature/popular_movies/presentation/movie_list.dart';
 
-class Home extends ConsumerWidget {
+class Home extends ConsumerStatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends ConsumerState<Home> {
+  @override
+  Widget build(BuildContext context) {
     bool shouldShowList = ref.watch(getMovieListStateProvider);
+
+    ref.listen<bool>(
+      shouldDisplayBottomSheet,
+      (_, next) {
+        if (next = true) {
+          _displayBottomSheet('error');
+        } else {
+          _displayBottomSheet('sns');
+        }
+      },
+    );
 
     return Scaffold(
       appBar: AppBar(
@@ -27,6 +44,31 @@ class Home extends ConsumerWidget {
           ],
         ),
       ),
+    );
+  }
+
+  void _displayBottomSheet(String errorMsg) async {
+    return showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 200,
+          color: Colors.amber,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(errorMsg),
+                ElevatedButton(
+                  child: const Text('Close BottomSheet'),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/feature/movie_details/domain/notifier/bottom_sheet_provider.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final shouldDisplayBottomSheet = StateProvider.autoDispose<bool>((ref) => true);
